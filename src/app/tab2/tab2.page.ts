d87faf28c2f291a05361b4f1c2362d87d3d5acb1
josephtascona <joseph.tascona@gmail.com>
2020-05-29 19:54:58 -0400

    Fully functioning on mobile

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ActionSheetController } from '@ionic/angular';
 import { PhotoService } from '../services/photo.service';
 
 @Component({
@@ -9,9 +9,31 @@ import { PhotoService } from '../services/photo.service';
 })
 export class Tab2Page {
 
-  constructor(public photoService: PhotoService) {}
+  constructor(public photoService: PhotoService, public actionSheetController: ActionSheetController) {}
 
   ngOnInit() {
     this.photoService.loadSaved();
+  }
+
+  public async showActionSheet(photo, position) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+         }
+      }]
+    });
+    await actionSheet.present();
   }
 }
