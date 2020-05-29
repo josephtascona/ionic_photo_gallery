37391d5d1cd03ed5cd441cae947194c0d8623b59
josephtascona <joseph.tascona@gmail.com>
2020-05-29 19:14:27 -0400

    Added functionality to save photo

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -7,8 +7,46 @@ const { Camera, Filesystem, Storage } = Plugins;
   providedIn: 'root'
 })
 export class PhotoService {
+  public photos: Photo[] = [];
 
   constructor() { }
+
+  private async savePicture(cameraPhoto: CameraPhoto) {
+    // Convert photo to base64 format, required by Filesystem Api to save
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    // Write the file to the data FileSystemDirectory
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+
+    // Use webPath to display the new image instead of base64 since it's
+    // already loaded into memory
+    return {
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath
+    };
+  }
+
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  })
 
   public async addNewToGallery() {
     // Take a photo
@@ -17,5 +55,19 @@ export class PhotoService {
       source: CameraSource.Camera,
       quality: 100
     });
+
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath
+    });
+
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photo.unshift(savedImageFile);
   }
+}
+
+interface Photo {
+  filepath: string;
+  webviewPath: string;
+  base64?: string;
 }
